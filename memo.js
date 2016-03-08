3cdebacf40b920b32878ea079600b816d430bc33
Charles Delestre <delestrecharles@gmail.com>
2016-03-08 14:14:36 +0100

    First commit
    Game commit

---
+++ memo.js
@@ -0,0 +1,169 @@
+var Gagner     = 0;
+var Count      = 0;
+var LastCarte  = null;
+var ModeDebug  = false;
+
+// 4 valeurs * 2 = 8 Cartes, 5 valeurs * 2 = 10 Cartes etc...
+var ArrayCartes = new Array("A", "B", "C", "D", "E", "F", "G", "H");
+var ValueCarte  = ArrayCartes.concat(ArrayCartes);
+var NbrValeur   = ValueCarte.length;
+var ArrayDonne  = new Array(NbrValeur);
+var Tirage      = true;
+var Tentative   = 0;
+var PairesOk    = 0;
+
+// Temporisateur en secondes
+var Tempor      = 1000;
+
+// 1ere Donne des Cartes
+window.onload = Donne;
+
+// Retourne toutes les Cartes (Coté Back)
+function Rejouer(){
+
+  objColTr = document.getElementById('TabCarte').rows;
+
+  for(i=0; i < objColTr.length; i++)
+  {
+    colTd = objColTr[i].cells
+    for(x=0; x < colTd.length; x++)
+    {
+    	colTd[x].style.backgroundImage = "";
+      colTd[x].className = "Back";
+    }
+  }
+  Count = 0;
+  document.getElementById('Rejouer').disabled = true;
+  document.getElementById('Solution').disabled = false;
+  Donne();
+}
+
+// Retourne toutes les cartes (Solution)
+function Solution(){
+
+  objColTr = document.getElementById('TabCarte').rows;
+
+  for(i=0; i < objColTr.length; i++)
+  {
+    colTd = objColTr[i].cells
+    for(x=0; x < colTd.length; x++)
+    {
+      colTd[x].style.backgroundImage = "url("+ArrayDonne[colTd[x].id]+".jpg)";
+      colTd[x].className = "Front";
+    }
+  }
+  Count = 0;
+  document.getElementById('Solution').disabled = true;
+  document.getElementById('Rejouer').disabled = false;
+}
+
+// Tirage Aléatoire des Cartes
+function Donne(){
+
+  var objMSG    = document.getElementById("MSG");
+  var objColTr  = document.getElementById('TabCarte').rows;
+  var NbrCartes = (objColTr[0].cells.length * objColTr.length);
+  var objDebug  = document.getElementById("DivDebug");
+
+  var OK        = true;
+  var Buffer    = "";
+  var Compteur  = 0;
+  
+  // Affiche Masque les infos de debuguage
+  objDebug.style.display = (ModeDebug) ? "block" : "none";
+
+  // Le Nbr de Cartes(TD) doit etre egale au Nbr de Valeurs
+  // Et doit être un Nbr Paire
+  if(NbrValeur == NbrCartes)
+  {
+    while(OK)
+    {
+      var Tirage = Math.floor(Math.random() * NbrCartes);
+      var reg    = new RegExp("#"+Tirage+":", "gi") ;
+
+      if (!reg.test(Buffer))
+      {
+        ArrayDonne[Compteur] = ValueCarte[Tirage];
+        Buffer += "#" + Tirage+ ":"; 
+
+        // Affiche le Buffer pour Debug Infos
+        objMSG.innerHTML = Buffer;
+        Compteur ++;
+        if(Compteur == NbrCartes) OK = false;
+      }
+    }
+  }
+}
+
+// Temporisateur Reset
+function ResetChoix(objID, objLast){
+
+  objID   = document.getElementById(objID);
+  objLast = document.getElementById(objLast);
+  
+  // Images Vide
+  objID.style.backgroundImage   = "";
+  objLast.style.backgroundImage = "";
+
+  // Classe Back
+  objID.className   = "Back";
+  objLast.className = "Back";
+  Tirage            = true;
+}
+
+// Click sur une Carte
+function Jouer(objID){
+
+	// La Carte ne doit pas être retournée
+  if(Tirage && objID.className != "Front")
+  {
+    Tirage = false;
+
+    if(LastCarte == null)
+    {
+      objID.className = "Front";
+      objID.style.backgroundImage = "url("+ArrayDonne[objID.id]+".jpg)";
+      LastCarte       = objID;
+      Tirage          = true;
+    }
+    else
+    {
+      if(LastCarte.id != objID.id && objID.className != "Front")
+      {
+        // Retourne la Carte
+        objID.className = "Front";
+        objID.style.backgroundImage = "url("+ArrayDonne[objID.id]+".jpg)";
+
+        // Si les deux Cartes sont différentes
+        if(ArrayDonne[objID.id] != ArrayDonne[LastCarte.id])
+        {
+          // Declenche le Tempo
+          a = setTimeout( "ResetChoix(" + objID.id + ", "+ LastCarte.id + ")", Tempor);
+          LastCarte       = null;
+        }
+        else
+        {
+          // Les Deux Cartes sont EGALE
+          LastCarte       = null;
+          Count ++;
+          PairesOk ++;
+          document.getElementById('paires').innerHTML = PairesOk;
+
+          // Si Tout Gagner (nbr de paires)
+          if(Count == (NbrValeur/2))
+          {
+            Gagner   ++;
+            document.getElementById('NbrGagner').innerHTML = Gagner;
+            document.getElementById('Rejouer').disabled = false;
+          }
+          Tirage = true;
+        }
+
+        Tentative ++;
+        document.getElementById('Try').innerHTML = Tentative;
+
+      }
+    }
+  }
+}
+
